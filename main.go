c8f17fc91044420cc6898a06080a250f8f81c1c4
MathxH Chen <brainfvck@foxmail.com>
2024-10-10 10:53:35 +0800

    Update CI yml to run unit tests with verbose output and exclude test directories

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"landui.com/ci-example/utils"
 )
@@ -10,5 +13,61 @@ func main() {
 	// Call the function
 	fmt.Println("Hello, World!")
 
-	fmt.Printf("Sum is: %d", utils.Add(1, 2))
+	go func() {
+		http.HandleFunc("/api/get", func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodGet {
+				http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+				return
+			}
+
+			response := map[string]interface{}{
+				"code": 200,
+				"msg":  "Hello, World",
+			}
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+		})
+
+		http.HandleFunc("/api/sum", func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodPost {
+				http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+				return
+			}
+
+			// Define a struct to parse the JSON body
+			var requestBody struct {
+				ParamA int `json:"a"`
+				ParamB int `json:"b"`
+			}
+
+			// Parse the JSON body
+			if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+				http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+				return
+			}
+
+			// Calculate the sum
+			sum := utils.Add(requestBody.ParamA, requestBody.ParamB)
+
+			// Create the response
+			response := map[string]interface{}{
+				"code": 200,
+				"msg":  "success",
+				"data": map[string]int{
+					"sum": sum,
+				},
+			}
+
+			// Set the Content-Type header and encode the response as JSON
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+
+		})
+
+		log.Println("Starting API server on :8080")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	// block forever
+	select {}
 }
